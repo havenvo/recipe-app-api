45cddef51811faebb3c0246c132885e0bc0c5bb3
Haven Vo <haven@enclave.vn>
2020-08-08 10:13:50 +0700

    Add tests for update recipe

--- app/recipe/tests/test_recipes_api.py
+++ app/recipe/tests/test_recipes_api.py
@@ -145,3 +145,36 @@ class PrivateRecipeTests(TestCase):
         self.assertEqual(len(ingredients), 2)
         self.assertIn(ingredient1, ingredients)
         self.assertIn(ingredient2, ingredients)
+
+    def test_partial_update_recipe(self):
+        recipe = create_same_recipe(user=self.user)
+        recipe.tags.add(create_sample_tag(user=self.user))
+        new_tag = create_sample_tag(user=self.user, name='Curry')
+
+        payload = {'title': 'New recipe name', 'tags': [new_tag.id]}
+        url = detail_url(recipe.id)
+        self.client.patch(url, payload)
+
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, payload['title'])
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags), 1)
+        self.assertIn(new_tag, tags)
+
+    def test_full_update_recipe(self):
+        recipe = create_same_recipe(user=self.user)
+        recipe.tags.add(create_sample_tag(user=self.user))
+        payload = {
+            'title': 'Spaghetti chicken',
+            'time_minutes': 25,
+            'price': 12.00
+        }
+        url = detail_url(recipe.id)
+        self.client.put(url, payload)
+
+        recipe.refresh_from_db()
+
+        self.assertEqual(recipe.title, payload['title'])
+        self.assertEqual(recipe.time_minutes, payload['time_minutes'])
+        self.assertEqual(recipe.price, payload['price'])
+        self.assertEqual(recipe.tags.all().count(), 0)
